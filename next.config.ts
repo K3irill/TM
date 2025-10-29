4d5be0c9d73b721c5546ec5a473abd3f0a6dcb0d
Kirill Kolesnichenko <razdor.2019@mail.ru>
2025-10-29 18:09:58 +0300

    chore: ignore ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from 'next'
-
-const nextConfig: NextConfig = {
-	/* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	typescript: {
+		// !! ВНИМАНИЕ !!
+		// Опасность: игнорирует все TypeScript ошибки
+		ignoreBuildErrors: true,
+	},
 }
 
-export default nextConfig
+module.exports = nextConfig
